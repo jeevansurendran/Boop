caa0ee466015ba4db7704ff5054be61164c2cdb8
Jeevan Surendran <jvns67@gmail.com>
2021-03-28 17:47:23 +0530

    Create Search Fragment
    Signed-off-by: Jeevan Surendran <jvns67@gmail.com>

---
+++ app/src/main/java/com/silverpants/instantaneous/ui/search/MainSearchFragment.kt
@@ -0,0 +1,6 @@
+package com.silverpants.instantaneous.ui.search
+
+import androidx.fragment.app.Fragment
+import com.silverpants.instantaneous.R
+
+class MainSearchFragment : Fragment(R.layout.fragment_main_search)
