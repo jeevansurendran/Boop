67ec99e7b6100dbfe3be7634c3f8c936dc0745fb
Jeevan Surendran <jvns67@gmail.com>
2021-04-15 15:05:27 +0530

    Add checks for userid
    Signed-off-by: Jeevan Surendran <jvns67@gmail.com>

--- app/src/main/java/com/silverpants/instantaneous/ui/auth/AuthUserIdFragment.kt
+++ app/src/main/java/com/silverpants/instantaneous/ui/auth/AuthUserIdFragment.kt
@@ -1,10 +1,9 @@
 package com.silverpants.instantaneous.ui.auth
 
 import android.os.Bundle
-import android.text.InputFilter
-import android.text.Spanned
 import android.view.View
 import android.widget.Button
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.textfield.TextInputLayout
@@ -24,8 +23,8 @@ class AuthUserIdFragment : Fragment(R.layout.fragment_auth_user_id) {
         val binding = FragmentAuthUserIdBinding.bind(view)
         val tilAuthUserId = binding.tilAuthUserId
         val btnAuthUserIdNext = binding.btnAuthUserIdNext
-        val userId = tilAuthUserId.editText?.text.toString().trim()
-        val userIdPattern: Regex = "[a-z][a-z_0-9]".toRegex()
+
+        val userIdPattern: Regex = "[a-z_0-9]{3,}".toRegex()
 
         viewModel.postUserId.observe(viewLifecycleOwner) {
             it?.let {
@@ -37,8 +36,10 @@ class AuthUserIdFragment : Fragment(R.layout.fragment_auth_user_id) {
                     }
                     is Result.Error -> {
                         if (it.exception is DocumentExistsException) {
-                            tilAuthUserId.error =
-                                "This user id is already taken, Try with another one"
+                            setInvalidFormat(
+                                tilAuthUserId, btnAuthUserIdNext,
+                                "This user id is already taken"
+                            )
                         }
                     }
                     else -> {
@@ -50,31 +51,34 @@ class AuthUserIdFragment : Fragment(R.layout.fragment_auth_user_id) {
         }
 
         btnAuthUserIdNext.setOnClickListener {
-            if (!userId.matches(userIdPattern)) {
-
+            if (!tilAuthUserId.editText?.text.toString().matches(userIdPattern)) {
+                setInvalidFormat(tilAuthUserId, btnAuthUserIdNext)
+                return@setOnClickListener
             }
             btnAuthUserIdNext.isEnabled = false
-            viewModel.postUserId(userId)
+            viewModel.postUserId(tilAuthUserId.editText?.text.toString())
         }
 
-        tilAuthUserId.editText?.filters = arrayOf(object : InputFilter {
-            override fun filter(
-                source: CharSequence?,
-                start: Int,
-                end: Int,
-                dest: Spanned?,
-                dstart: Int,
-                dend: Int
-            ): CharSequence? {
-                return source?.filter { source.subSequence(start, end).matches(userIdPattern) }
+        binding.tilAuthUserId.editText?.doOnTextChanged { text, start, before, count ->
+            if (!tilAuthUserId.editText?.text.toString().matches(userIdPattern)) {
+                setInvalidFormat(tilAuthUserId, btnAuthUserIdNext)
+                return@doOnTextChanged
             }
-
-        })
-
+            setValidFormat(tilAuthUserId, btnAuthUserIdNext)
+        }
     }
 
-    fun setInvalidFormat(tilAuth: TextInputLayout, btn: Button) {
-        tilAuth.editText?.error = "User Id can only contain small letters, alphabets and "
+    private fun setInvalidFormat(
+        tilAuth: TextInputLayout,
+        btn: Button,
+        message: String = "User Id can only contain lowercase letters, underscores and must be longer than 3 characters."
+    ) {
+        tilAuth.error = message
+        btn.isEnabled = false
+    }
 
+    private fun setValidFormat(tilAuth: TextInputLayout, btn: Button) {
+        tilAuth.error = null
+        btn.isEnabled = true
     }
 }
